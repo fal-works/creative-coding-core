7bc07ad99f3aa5bd1153199f94e54f46a391607f
FAL <falworks.contact@gmail.com>
2019-09-19 20:25:03 +0900

    fix: export missing modules

--- src/geometry/index.ts
+++ src/geometry/index.ts
@@ -2,7 +2,13 @@ import * as Angle from "./angle";
 export { Angle };
 
 export { RectangleRegion } from "./rectangle-region";
+import * as RectangleRegionUtility from "./rectangle-region-utility";
+export { RectangleRegionUtility };
 
 export { RectangleSize } from "./rectangle-size";
+import * as RectangleSizeUtility from "./rectangle-size-utility";
+export { RectangleSizeUtility };
 
 export { Vector2D } from "./vector-2d";
+import * as Vector2DUtility from "./vector-2d-utility";
+export { Vector2DUtility };
