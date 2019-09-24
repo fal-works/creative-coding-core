8037b58ca6659bd4bef8b2a2db9af38b68951f1e
FAL <falworks.contact@gmail.com>
2019-09-24 23:32:45 +0900

    Add Angle.fromDegrees(), toDegrees()

--- src/geometry/angle.ts
+++ src/geometry/angle.ts
@@ -1,5 +1,8 @@
 import { TWO_PI } from "../numeric/math";
 
+const DEGREES_TO_RADIANS_FACTOR = TWO_PI / 360;
+const RADIANS_TO_DEGREES_FACTOR = 360 / TWO_PI;
+
 export const createArray = (resolution: number): readonly number[] => {
   const array: number[] = new Array(resolution);
   const interval = TWO_PI / resolution;
@@ -7,3 +10,7 @@ export const createArray = (resolution: number): readonly number[] => {
 
   return array;
 };
+
+export const fromDegrees = (degrees: number): number => DEGREES_TO_RADIANS_FACTOR * degrees;
+
+export const toDegrees = (radians: number): number => RADIANS_TO_DEGREES_FACTOR * radians;
