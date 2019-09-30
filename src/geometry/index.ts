548880e4b860e6575b495521d381a10f6ae300a4
FAL <falworks.contact@gmail.com>
2019-09-30 09:19:24 +0900

    refactor & add methods for Vector2D

--- src/geometry/index.ts
+++ src/geometry/index.ts
@@ -10,5 +10,8 @@ export { RectangleSize };
 import * as Vector2D from "./vector-2d";
 export { Vector2D };
 
+import * as MutableVector2D from "./vector-2d-mutable";
+export { MutableVector2D };
+
 import * as Bezier from "./bezier";
 export { Bezier };
--- src/geometry/vector-2d.ts
+++ src/geometry/vector-2d.ts
@@ -1,4 +1,4 @@
-import { Mutable } from "../types/mutable";
+import { sq } from "../numeric/math";
 
 /**
  * A readonly 2D vector.
@@ -11,30 +11,6 @@ export interface Unit {
 export const zero: Unit = {
   x: 0,
   y: 0
-};
-
-export const setMutable = (
-  vector: Mutable<Unit>,
-  x: number,
-  y: number
-): Mutable<Unit> => {
-  vector.x = x;
-  vector.y = y;
-
-  return vector;
-};
-
-/**
- * Creates a new vector by adding two vectors.
- * @param a
- * @param b
- * @return new `Vector2D`.
- */
-export const add = (a: Unit, b: Unit): Unit => {
-  return {
-    x: a.x + b.x,
-    y: a.y + b.y
-  };
 };
 
 /**
@@ -51,20 +27,70 @@ export const fromPolar = (length: number, angle: number): Unit => {
 };
 
 /**
+ * Creates a new vector by adding two vectors.
+ * @param a
+ * @param b
+ * @return new `Vector2D`.
+ */
+export const add = (a: Unit, b: Unit): Unit => {
+  return {
+    x: a.x + b.x,
+    y: a.y + b.y
+  };
+};
+
+/**
+ * Creates a new vector by adding cartesian coordinates.
+ * @param vector
+ * @param x
+ * @param y
+ * @return new `Vector2D`.
+ */
+export const addCartesian = (vector: Unit, x: number, y: number): Unit => {
+  return {
+    x: vector.x + x,
+    y: vector.y + y
+  };
+};
+
+/**
  * Creates a new vector by adding polar coordinates.
  * @param vector
  * @param length
  * @param angle
  * @return new `Vector2D`.
  */
-export const addPolar = (
-  vector: Unit,
-  length: number,
-  angle: number
-): Unit => {
+export const addPolar = (vector: Unit, length: number, angle: number): Unit => {
   return {
     x: vector.x + length * Math.cos(angle),
     y: vector.y + length * Math.sin(angle)
+  };
+};
+
+/**
+ * Creates a new vector by subtracting `b` from `a`.
+ * @param a
+ * @param b
+ * @return new `Vector2D`.
+ */
+export const subtract = (a: Unit, b: Unit): Unit => {
+  return {
+    x: a.x - b.x,
+    y: a.y - b.y
+  };
+};
+
+/**
+ * Creates a new vector by subtracting cartesian coordinates.
+ * @param vector
+ * @param x
+ * @param y
+ * @return new `Vector2D`.
+ */
+export const subtractCartesian = (vector: Unit, x: number, y: number): Unit => {
+  return {
+    x: vector.x - x,
+    y: vector.y - y
   };
 };
 
@@ -85,3 +111,11 @@ export const subtractPolar = (
     y: vector.y - length * Math.sin(angle)
   };
 };
+
+export const distanceSquared = (vectorA: Unit, vectorB: Unit): number =>
+  sq(vectorB.x - vectorA.x) + sq(vectorB.y - vectorA.y);
+
+export const distance = (vectorA: Unit, vectorB: Unit): number =>
+  Math.sqrt(distanceSquared(vectorA, vectorB));
+
+export const toStr = (vector: Unit): string => `{x:${vector.x},y:${vector.y}}`;
