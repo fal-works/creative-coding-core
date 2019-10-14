b78871e146d25303683fcb6e8679484033e4ee0d
FAL <falworks.contact@gmail.com>
2019-10-14 20:08:49 +0900

    update libraries & improve ESLint config

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,23 @@
 import typescript from "rollup-plugin-typescript2";
+import cleanup from "rollup-plugin-cleanup";
+
+const version = "0.1.7";
+const cleanBuild = false;
 
 const moduleName = "creative-coding-core";
 const umdName = "CreativeCodingCore";
-const version = "0.1.7";
 const year = "2019";
 const description = `* Utility library that might be useful for creative coding.`;
 
 const bannerComment = `/**
 ${description}
-* GitHub repository: {@link https://github.com/fal-works/${moduleName}}
-* @module ${moduleName}
-* @copyright ${year} FAL
-* @author FAL <contact@fal-works.com>
-* @license MIT
-* @version ${version}
-*/
+ * GitHub repository: {@link https://github.com/fal-works/${moduleName}}
+ * @module ${moduleName}
+ * @copyright ${year} FAL
+ * @author FAL <contact@fal-works.com>
+ * @license MIT
+ * @version ${version}
+ */
 `;
 
 export default {
@@ -38,7 +41,13 @@ export default {
   ],
   plugins: [
     typescript({
-      useTsconfigDeclarationDir: true
+      useTsconfigDeclarationDir: true,
+      celan: cleanBuild
+    }),
+    cleanup({
+      comments: /^\*\*/, // preserve multiline comments
+      sourcemap: false,
+      extensions: ["ts"]
     })
   ]
 };
