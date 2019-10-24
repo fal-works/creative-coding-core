724cbd526620fab00469753f0e8a475d8ee43c42
FAL <falworks.contact@gmail.com>
2019-10-25 06:12:16 +0900

    rename and export Angle constants

--- src/geometry/angle.ts
+++ src/geometry/angle.ts
@@ -4,8 +4,8 @@ export const PI = Math.PI;
 export const HALF_PI = PI / 2;
 export const TWO_PI = 2 * PI;
 
-const DEGREES_TO_RADIANS_FACTOR = TWO_PI / 360;
-const RADIANS_TO_DEGREES_FACTOR = 360 / TWO_PI;
+export const DEGREES_TO_RADIANS = TWO_PI / 360;
+export const RADIANS_TO_DEGREES = 360 / TWO_PI;
 
 export const createArray = (resolution: number): readonly number[] => {
   const array: number[] = new Array(resolution);
@@ -16,10 +16,10 @@ export const createArray = (resolution: number): readonly number[] => {
 };
 
 export const fromDegrees = (degrees: number): number =>
-  DEGREES_TO_RADIANS_FACTOR * degrees;
+  DEGREES_TO_RADIANS * degrees;
 
 export const toDegrees = (radians: number): number =>
-  RADIANS_TO_DEGREES_FACTOR * radians;
+  RADIANS_TO_DEGREES * radians;
 
 /**
  * Calculates the angle in radians from origin to `position`.
