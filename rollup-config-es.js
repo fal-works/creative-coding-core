80237c1d627c6232b0a31d9a572d41b44b418fe3
FAL <falworks.contact@gmail.com>
2019-11-30 17:52:52 +0900

    improve build scripts

---
+++ rollup-config-es.js
@@ -0,0 +1,19 @@
+import {
+  distributionDirectory,
+  moduleName,
+  bannerComment,
+  input,
+  createPlugins
+} from "./rollup-config-common";
+
+export default {
+  input,
+  output: {
+    file: `${distributionDirectory}/${moduleName}.mjs`,
+    format: "es",
+    sourcemap: false,
+    banner: bannerComment,
+    preferConst: true
+  },
+  plugins: createPlugins({ declaration: true })
+};
---
+++ rollup-config-umd.js
@@ -0,0 +1,21 @@
+import {
+  distributionDirectory,
+  moduleName,
+  umdName,
+  bannerComment,
+  input,
+  createPlugins
+} from "./rollup-config-common";
+
+export default {
+  input,
+  output: {
+    file: `${distributionDirectory}/${moduleName}.js`,
+    format: "umd",
+    name: umdName,
+    sourcemap: false,
+    banner: bannerComment,
+    preferConst: true
+  },
+  plugins: createPlugins()
+};
