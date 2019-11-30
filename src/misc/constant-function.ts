96af0d589f326df3c115b79c0cd2f91205109772
FAL <falworks.contact@gmail.com>
2019-11-30 12:37:48 +0900

    add ConstantFunction.runSelf

--- src/misc/constant-function.ts
+++ src/misc/constant-function.ts
@@ -3,3 +3,9 @@ export const returnUndefined = () => undefined;
 export const returnNull = () => null;
 export const returnZero = () => 0;
 export const returnOne = () => 1;
+
+/**
+ * Runs `callback` without any arguments.
+ * @param callback - Any function that can be run without any arguments.
+ */
+export const runSelf = <T>(callback: () => T) => callback();
