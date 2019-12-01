83c738568852d7cf5922b305345ab9cae30f0625
FAL <falworks.contact@gmail.com>
2019-12-01 22:37:37 +0900

    v0.2.1

--- rollup-config-common.js
+++ rollup-config-common.js
@@ -3,7 +3,7 @@ import cleanup from "rollup-plugin-cleanup";
 
 // ----------------------------------------------------------------------------
 
-const version = "0.2.0";
+const version = "0.2.1";
 const cleanBuild = true;
 
 const moduleName = "creative-coding-core";
