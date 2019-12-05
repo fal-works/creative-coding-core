3060e0abf614e646ca91be105916806492580de8
FAL <falworks.contact@gmail.com>
2019-12-06 02:15:29 +0900

    improve Tween

--- src/misc/tween/tween.ts
+++ src/misc/tween/tween.ts
@@ -4,25 +4,57 @@ import * as Timer from "../timer";
 export type Parameters = {
   readonly start: number;
   readonly end: number;
-  readonly duration: number;
   readonly easing?: Easing.FunctionUnit;
 };
 
 /**
  * Creates a `Timer` instance for tweening value using `setValue()`.
- * @param parameters `start`, `end`, `duration` and `easing`(linear by default).
+ * @param setValue A function that receives the tweened value.
+ * @param duration Duration frame count.
+ * @param parameters `start`, `end` and `easing`(linear by default).
  * @returns New `Timer` instance.
  */
 export const create = (
   setValue: (value: number) => void,
+  duration: number,
   parameters: Parameters
 ) => {
-  const { start, end, duration } = parameters;
+  const { start, end } = parameters;
   const ease = parameters.easing || Easing.linear;
 
   return Timer.create({
     duration,
     onProgress: progress =>
       setValue(Numeric.lerp(start, end, ease(progress.ratio)))
+  });
+};
+
+/**
+ * Creates a `Timer` instance for tweening value using `setValue()`.
+ * The parameters are evaluated at the timing when the timer starts.
+ * @param setValue A function that receives the tweened value.
+ * @param duration Duration frame count.
+ * @param getParameters A function that returns `start`, `end` and `easing`(linear by default).
+ * @returns New `Timer` instance.
+ */
+export const setCreate = (
+  setValue: (value: number) => void,
+  duration: number,
+  getParameters: () => Parameters
+) => {
+  let startValue: number;
+  let endValue: number;
+  let ease: Easing.FunctionUnit;
+
+  return Timer.create({
+    duration,
+    onStart: () => {
+      const { start, end, easing } = getParameters();
+      startValue = start;
+      endValue = end;
+      ease = easing || Easing.linear;
+    },
+    onProgress: progress =>
+      setValue(Numeric.lerp(startValue, endValue, ease(progress.ratio)))
   });
 };
--- src/misc/tween/vector-2d.ts
+++ src/misc/tween/vector-2d.ts
@@ -4,31 +4,63 @@ import * as Timer from "../timer";
 
 export type Parameters = {
   readonly target: Vector2D.Unit;
-  readonly duration: number;
   readonly easing?: Easing.FunctionUnit;
 };
 
 /**
- * Creates a `Timer` instance for tweening `vector`.
- * The initial value of `vector` is evaluated at the timing when the timer starts.
+ * Creates a `Timer` instance for tweening `vector` from the current values.
+ * @param vector The vector to tween.
+ * @param duration Duration frame count.
  * @param parameters `target`, `duration` and `easing`(linear by default).
  * @returns New `Timer` instance.
  */
 export const create = (
   vector: Vector2D.Mutable.Unit,
+  duration: number,
   parameters: Parameters
 ) => {
-  const { duration } = parameters;
-
-  let startX: number, startY: number;
+  const { x: startX, y: startY } = vector;
   const { x: endX, y: endY } = parameters.target;
 
   const ease = parameters.easing || Easing.linear;
 
   return Timer.create({
     duration,
+    onProgress: progress => {
+      const ratio = ease(progress.ratio);
+      Vector2D.Mutable.setCartesian(
+        vector,
+        Numeric.lerp(startX, endX, ratio),
+        Numeric.lerp(startY, endY, ratio)
+      );
+    }
+  });
+};
+
+/**
+ * Creates a `Timer` instance for tweening `vector`.
+ * The starting values of `vector` and the parameters are evaluated at the timing when the timer starts.
+ * @param vector The vector to tween.
+ * @param duration Duration frame count.
+ * @param parameters `target`, `duration` and `easing`(linear by default).
+ * @returns New `Timer` instance.
+ */
+export const setCreate = (
+  vector: Vector2D.Mutable.Unit,
+  duration: number,
+  getParameters: () => Parameters
+) => {
+  let startX: number, startY: number;
+  let endX: number, endY: number;
+  let ease: Easing.FunctionUnit;
+
+  return Timer.create({
+    duration,
     onStart: () => {
+      const { target, easing } = getParameters();
       ({ x: startX, y: startY } = vector);
+      ({ x: endX, y: endY } = target);
+      ease = easing || Easing.linear;
     },
     onProgress: progress => {
       const ratio = ease(progress.ratio);
