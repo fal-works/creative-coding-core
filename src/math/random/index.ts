c4c45e7a4f5bff32eb56a6f492240293a3a43720
FAL <falworks.contact@gmail.com>
2019-12-18 02:11:00 +0900

    fix export of Random

--- src/math/random/index.ts
+++ src/math/random/index.ts
@@ -1,5 +1,6 @@
 export * from "./base";
 export * from "./random";
+export * from "./misc";
 
 import * as Integer from "./integer";
 export { Integer };
@@ -9,6 +10,9 @@ export { Discrete };
 
 import * as Arrays from "./arrays";
 export { Arrays };
+
+import * as Curved from "./curved";
+export { Curved };
 
 import * as FullName from "./full-name";
 export { FullName };
