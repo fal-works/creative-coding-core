6941016379c9c6e4cf2e280d4e3955f2a8c81b87
FAL <falworks.contact@gmail.com>
2020-05-07 20:28:06 +0900

    add comments

--- src/ds/lazy.ts
+++ src/ds/lazy.ts
@@ -1,8 +1,14 @@
+/**
+ * A data unit of which value is lazily created.
+ */
 export interface Unit<T> {
   value: T | undefined;
   readonly factory: () => T;
 }
 
+/**
+ * @returns New `Lazy` unit with `factory`.
+ */
 export const create = <T>(factory: () => T): Unit<T> => {
   return {
     value: undefined,
@@ -10,9 +16,17 @@ export const create = <T>(factory: () => T): Unit<T> => {
   };
 };
 
+/**
+ * Returns the value of `object`.
+ * Creates the value if not yet created.
+ */
 export const get = <T>(object: Unit<T>) =>
   object.value || (object.value = object.factory());
 
+/**
+ * Clears the value of `object`.
+ * `object.factory` still remains, so the value can be created later again.
+ */
 export const clear = <T>(object: Unit<T>) => {
   object.value = undefined;
 };
